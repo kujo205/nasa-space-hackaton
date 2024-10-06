0bee48cf3ecd3a825d97bd45f465d69d92c87861
kujo205 <kuc8301@gmail.com>
2024-10-06 23:15:09 +0300

    feat: adapted about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -23,45 +23,55 @@ const members = [
   },
 ];
 
+const atricles = [
+  {
+    title: "What does our app do?",
+    description:
+      'Our app helps curious space researchers track the location of Landsat 8/9 satellites. Users can input their coordinates and choose whether to get historical images of the point they chose or photos of the soonest satellite pass. After entering all the details and clicking "submit", they will receive an email with all the data about their request (including location and max cloud coverage) and the type of photo they selected.  They will also receive a pinpointed pixel if they choose an option with the closest satellite pass. Besides receiving an email of satellite pass they are going to receive a notification on "n" day before it passes.\n',
+  },
+  {
+    title: "About Us",
+    description:
+      "We are a team of ambitious Ukrainian software engineers and product\n        analysts dedicated to our careers and studying at the KPI University. We\n        view studying as a main objective of our lives and so, dedicate a hefty\n        amount of time to this very purpose. We are also very keen on the topic\n        of space, which is why this contest drew our attention. Additionally, we\n        believe it's important to expand the boundaries of our knowledge, so we\n        decided to utilize our skills to make the lives of Space explorers\n        easier.",
+  },
+];
+
 export default function Page() {
   return (
-    <div className="max-w-4xl w-full mx-auto min-h-screen items-center justify-items-center min-h-screen">
-      <h2 className="font-bold text-3xl mt-20">About Us</h2>
-      <p className="mt-2 text-lg">
-        We are a team of ambitious Ukrainian software engineers and product
-        analysts dedicated to our careers and studying at the KPI University. We
-        view studying as a main objective of our lives and so, dedicate a hefty
-        amount of time to this very purpose. We are also very keen on the topic
-        of space, which is why this contest drew our attention. Additionally, we
-        believe it's important to expand the boundaries of our knowledge, so we
-        decided to utilize our skills to make the lives of Space explorers
-        easier.
-      </p>
+    <div className="px-5 max-w-4xl w-full mx-auto min-h-screen items-center justify-items-center min-h-screen">
+      {atricles.map((article) => (
+        <article key={article.title}>
+          <h2 className="font-bold text-3xl max-md:text-2xl mt-10">
+            {article.title}
+          </h2>
+          <p className="mt-2 max-md:text-sm text-lg">{article.description}</p>
+        </article>
+      ))}
 
       <div className="flex mt-10 flex-col gap-6">
         {members.map((member) => (
-          <Card key={member.photo} className="bg-gray-800">
-            <CardContent className="p-4 flex flex-col items-center">
-              <div className="w-40 h-40 rounded-full overflow-hidden mb-4">
-                <Image
-                  src={member.photo}
-                  alt={`Team Member ${member.name}`}
-                  width={160}
-                  height={160}
-                  className="object-cover"
-                />
-              </div>
-              <a href={member.linked}>
+          <a href={member.linked} key={member.photo}>
+            <Card className="bg-gray-800 hover:bg-gray-700">
+              <CardContent className="p-4 flex flex-col items-center">
+                <div className="w-40 h-40 rounded-full overflow-hidden mb-4">
+                  <Image
+                    src={member.photo}
+                    alt={`Team Member ${member.name}`}
+                    width={160}
+                    height={160}
+                    className="object-cover"
+                  />
+                </div>
                 <Button
                   variant="link"
                   className="text-xl text-white font-semibold"
                 >
                   {member.name}
                 </Button>
-              </a>
-              <p className="text-sm text-gray-400">{member.role}</p>
-            </CardContent>
-          </Card>
+                <p className="text-sm text-gray-400">{member.role}</p>
+              </CardContent>
+            </Card>
+          </a>
         ))}
       </div>
     </div>
